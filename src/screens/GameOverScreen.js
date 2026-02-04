28545d0b6917373587b35714954552f0163e4534
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-04 11:06:44 -0500

    initial game screens

---
+++ src/screens/GameOverScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const GameOverScreen = () => {
+  return (
+    <View>
+      <Text>GameOverScreen</Text>
+    </View>
+  );
+};
+
+export default GameOverScreen;
