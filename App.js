4e1da218511407c16da9ebfbb71913e4554adbe6
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-04 12:46:06 -0500

    added linear gradient and background image

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
-import { StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { ImageBackground, StyleSheet } from 'react-native';
+import backgroundImage from './assets/images/background.png';
 import StartGameScreen from './src/screens/StartGameScreen';
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient style={styles.rootScreen} colors={['#4e0329', '#ddb52f']}>
+      <ImageBackground
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+        resizeMode="cover"
+        source={backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
